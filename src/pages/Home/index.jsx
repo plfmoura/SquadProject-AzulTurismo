b5c689f2df84e2e07483265ef753f5989760b2f8
qu4resm4 <117126578+qu4resm4@users.noreply.github.com>
2023-02-27 11:12:26 -0300

    add(title/navBar)animations/styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import style from './home.module.css'
 
 export default function Home() {
   const state = useSelector((state) => state);
   const { products } = state.shopping;
   return (
-    <>
+    <div className="Home" style={{color: "#fff"}}>
+    <div className={style.background}>
+      <div className={style.titleContainer}>
+        <p>Conheça o</p>
+        <span>RIO</span>
+        <p>com nossos</p>
+        <span>Roteiros</span>
+      </div>
+    </div>
       <h1>Pagina de Home</h1>
       {products.map((producto) => (
         <li key={producto.id}>{producto.name}</li>
       ))}
-    </>
+    </div>
   );
 }
