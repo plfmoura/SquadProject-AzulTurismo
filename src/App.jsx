a4a436362215729e46a436b1f5a0f8063af2343f
plfmoura <plfmoura96@gmail.com>
2023-02-27 18:40:46 -0300

    add(home)search/card components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import { actualizarProductos } from "./reducer/shoopingReducer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const dispatch = useDispatch();
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,27 +1,43 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import style from './home.module.css'
+import TourCard from "../../components/TourCard";
+import style from "./home.module.css";
+import SearchInput from "./SearchInput";
 
 export default function Home() {
   const state = useSelector((state) => state);
   const { products } = state.shopping;
   return (
-    <div className="Home" style={{color: "#fff"}}>
-    <div className={style.background}>
-      <div className={style.titleContainer}>
-        <p>Conheça o</p>
-        <span>RIO</span>
-        <p>com nossos</p>
-        <span>Roteiros</span>
-      </div>
-      <div className={style.creativeContainer}>
-        <img src="/src/assets/home/men.png" alt="" />
-      </div>
-    </div>
-      <h1>Pagina de Home</h1>
-      {products.map((producto) => (
-        <li key={producto.id}>{producto.name}</li>
-      ))}
+    <div className="Home">
+      <header className={style.background}>
+        <div className={style.titleContainer}>
+          <p>Conheça o</p>
+          <span>RIO</span>
+          <p>com nossos</p>
+          <span>Roteiros</span>
+        </div>
+        <div className={style.creativeContainer}>
+          <img src="/src/assets/home/men.png" alt="" />
+        </div>
+      </header>
+      <section className={style.searchInputContainer}>
+        <SearchInput />
+      </section>
+      <section className={style.servicesContainer}>
+        <h3>Passeios mais Populares</h3>
+        <div className={style.servicesSlider}>
+          {products.map((tour) => (
+            <TourCard 
+              key={tour.id} 
+              title={tour.name} 
+              location={tour.located} 
+              price={tour.price} 
+              image={tour.imagens[0]}
+              />
+            ))
+          }
+        </div>
+      </section>
     </div>
   );
 }
