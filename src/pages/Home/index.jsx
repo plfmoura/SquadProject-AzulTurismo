f215dc54f08a12d52208eb71e68fdae35da16847
Pedro Moura <107577096+plfmoura@users.noreply.github.com>
2023-02-28 01:18:58 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -18,7 +18,7 @@ export default function Home() {
           <span>Roteiros</span>
         </div>
         <div className={style.creativeContainer}>
-          <img src="../src/assets/home/men.png" alt="" />
+          <img src="src/assets/home/men.png" alt="" />
         </div>
       </header>
       <section className={style.searchInputContainer}>
