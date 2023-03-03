e31a2216e63f09630c45c5fd181fe8cd5fe69b88
Guilherme Felipe Dos Santos <FELLIPEGUILHERME37@GMAIL.COM>
2023-03-03 00:10:03 -0300

    Footer
    Alterações: fundo do footer, endentação do código, modificações do css nas classeNames: EstiloFooter, rodaTitle, rodaList

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -1,75 +1,74 @@
 import styles from '../../components/Rodape/rodape.module.css';
-import {  FaFacebookSquare, FaInstagram} from "react-icons/fa";
+import { FaFacebookSquare, FaInstagram } from "react-icons/fa";
 
 
-function Rodape(){
-    return(
-        <>
-<footer className={styles.estiloFooter}>
-<h1>Explore outras opções em Rio de Janeiro</h1>
-<div className={styles.rodaList}>
-<div className={styles.rodaItem}>
-<ul>
-<li><a href="">Angra dos Reis</a></li>
-<li><a href="">Paraty</a></li>
-<li><a href="">Arraial do Cabo</a></li>
- 
-</ul>
- </div>
-
- <div className={styles.rodaItem}>
-<ul>
-<li><a href="">Angra dos Reis</a></li>
-<li><a href="">Paraty</a></li>
-<li><a href="">Arraial do Cabo</a></li>
- 
-</ul>
- </div>
-
- <div className={styles.rodaItem}>
-<ul>
-<li><a href="">Angra dos Reis</a></li>
-<li><a href="">Paraty</a></li>
-<li><a href="">Arraial do Cabo</a></li>
- 
-</ul>
- </div>
+function Rodape() {
+  return (
 
 
- <div className={styles.rodaItem}>
-<ul>
-<li><a href="">Angra dos Reis</a></li>
-<li><a href="">Paraty</a></li>
-<li><a href="">Arraial do Cabo</a></li>
- 
-</ul>
- </div>
+    <footer className={styles.estiloFooter}>
+      <h1 className={styles.rodaTitle}>Explore outras opções em Rio de Janeiro</h1>
+      <div className={styles.rodaList}>
+        <div className={styles.rodaItem}>
+          <ul>
+            <li><a href="">Angra dos Reis</a></li>
+            <li><a href="">Paraty</a></li>
+            <li><a href="">Arraial do Cabo</a></li>
+          </ul>
+        </div>
+
+        <div className={styles.rodaItem}>
+          <ul>
+            <li><a href="">Angra dos Reis</a></li>
+            <li><a href="">Paraty</a></li>
+            <li><a href="">Arraial do Cabo</a></li>
+          </ul>
+        </div>
+
+        <div className={styles.rodaItem}>
+          <ul>
+            <li><a href="">Angra dos Reis</a></li>
+            <li><a href="">Paraty</a></li>
+            <li><a href="">Arraial do Cabo</a></li>
+
+          </ul>
+        </div>
 
 
-</div>
+        <div className={styles.rodaItem}>
+          <ul>
+            <li><a href="">Angra dos Reis</a></li>
+            <li><a href="">Paraty</a></li>
+            <li><a href="">Arraial do Cabo</a></li>
 
-   <div className={styles.textoFinal}>
-  <p>&copy;2023Azul Tour, inc.</p> 
-  <div className={styles.textoLink}>
-<li><a href="">Privacidade</a></li>
-<li><a href="">Termos</a></li> 
-<li><a href="">Mapa do site</a></li>  
-<li><a href="">Informações da Empresa</a></li> 
-  </div>
-  <div className={styles.socialFinal}>
-<p className={styles.txtFim}>Portugues(BR) R$BRL</p>
-<div className={styles.socialItem}>
-<p className={styles.redeUm}><a href=""><FaFacebookSquare /></a></p>
-</div>
-<div className={styles.socialItem}>
-<p className={styles.redeDois}><a href=""><FaInstagram /></a></p>
-</div>
-  </div>
-  </div>
-    
-</footer>
-        </>
-    )
+          </ul>
+        </div>
+
+
+      </div>
+
+      <div className={styles.textoFinal}>
+        <p>&copy;2023Azul Tour, inc.</p>
+        <div className={styles.textoLink}>
+          <li><a href="">Privacidade</a></li>
+          <li><a href="">Termos</a></li>
+          <li><a href="">Mapa do site</a></li>
+          <li><a href="">Informações da Empresa</a></li>
+        </div>
+        <div className={styles.socialFinal}>
+          <p className={styles.txtFim}>Portugues(BR) R$BRL</p>
+          <div className={styles.socialItem}>
+            <p className={styles.redeUm}><a href=""><FaFacebookSquare /></a></p>
+          </div>
+          <div className={styles.socialItem}>
+            <p className={styles.redeDois}><a href=""><FaInstagram /></a></p>
+          </div>
+        </div>
+      </div>
+
+    </footer>
+
+  )
 }
 
 export default Rodape
