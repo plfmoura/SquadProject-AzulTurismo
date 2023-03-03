d332b3e2de17a1a6e78c4745f4f82347a6ca0d29
plfmoura <plfmoura96@gmail.com>
2023-03-03 17:49:54 -0300

    add(about)styles/map/section

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
-import style from './aboutUs.module.css'
-
+import React, { useState } from "react";
+import style from "./aboutUs.module.css";
+import Button from "../../components/Button";
+import GoogleMaps from "../../components/GoogleMaps";
 
 export default function About() {
   const [nome, setNome] = useState("");
@@ -16,61 +17,94 @@ export default function About() {
   return (
     // Detalhes da empresa parte A
     <div className={style.aboutContainer}>
-      <section className={style.section}>
+      <section className={style.primeiraSection}>
         <div>
           <h1>Detalhes da empresa</h1>
-          <p className={style.des}>
-            A nossa empresa atua no mercado desde 2023, sendo especializada em passeios turísticos em todo o Rio de Janeiro.
-
-            Prezamos pela praticidade e excelência na experiência do cliente, com atendimento diferenciado e uma frota preparada para proporcionar a melhor viagem para você, amigos e familiares.
-
-            Nossa equipe tem experiência internacional e realiza treinamentos constantes, o que nos possibilita oferecer os mais altos padrões de atendimento,
-            segurança e qualidade, seja em Transportes, Receptivos, City Tours, Transporte Executivo ou VIP.
-
-            Nossa missão é conectar pessoas aos seus destinos para que elas colecionem experiências incríveis e belas recordações!
+          <p className={style.description}>
+            A nossa empresa atua no mercado desde 2023, sendo especializada em
+            passeios turísticos em todo o Rio de Janeiro.
+            <br />
+            <br />
+            Prezamos pela praticidade e excelência na experiência do cliente,
+            com atendimento diferenciado e uma frota preparada para proporcionar
+            a melhor viagem para você, amigos e familiares.
+            <br />
+            <br />
+            Nossa equipe tem experiência internacional e realiza treinamentos
+            constantes, o que nos possibilita oferecer os mais altos padrões de
+            atendimento, segurança e qualidade, seja em Transportes, Receptivos,
+            City Tours, Transporte Executivo ou VIP.
+            <br />
+            <br />
+            Nossa missão é conectar pessoas aos seus destinos para que elas
+            colecionem experiências incríveis e belas recordações!
           </p>
+
+          <h3>Nosso escritório no Porto Maravalley</h3>
+          <div>
+            <GoogleMaps 
+              lat={-22.897052998253393} 
+              long={-43.20352553999137}
+              setclass={style.mapsContainer}
+            />
+            <p>Av. Prof. Pereira Reis, 76 - Santo Cristo, Rio de Janeiro - RJ, 20220-800, Brasil</p>
+          </div>
         </div>
-        <form onSubmit={handleSubmit} className={style.form}>
-        <h2>Fale conosco</h2>
-          <label>
-            Nome:
+        <form onSubmit={handleSubmit} className={style.formContainer}>
+          <h2>Fale conosco</h2>
+          <div>
+            <label>Nome</label>
             <input
               type="text"
               value={nome}
               onChange={(event) => setNome(event.target.value)}
             />
-          </label>
-          <br />
-          <label>
-            Email:
+          </div>
+          <div>
+            <label>Email</label>
             <input
               type="email"
               value={email}
               onChange={(event) => setEmail(event.target.value)}
             />
-          </label>
-          <br />
-          <label>
-            Telefone:
+          </div>
+          <div>
+            <label>Telefone</label>
             <input
               type="tel"
               value={telefone}
               onChange={(event) => setTelefone(event.target.value)}
             />
-          </label>
-          <br />
-          <label>
-            Mensagem:
+          </div>
+          <div>
+            <label>Selecione um setor</label>
+            <select
+              value={telefone}
+              onChange={(event) => setTelefone(event.target.value)}
+            >
+              <option value="default">...</option>
+              <option value="suggestion">Sugestão</option>
+              <option value="congratz">Agradecimento</option>
+              <option value="forget">Esqueci algo</option>
+              <option value="claim">Reclamação</option>
+            </select>
+          </div>
+          <div>
+            <label>Mensagem</label>
             <textarea
               value={mensagem}
               onChange={(event) => setMensagem(event.target.value)}
             />
-          </label>
-          <br />
-          <button type="submit">Enviar mensagem</button>
+          </div>
+          <Button type="submit" text="Enviar mensagem" />
         </form>
       </section>
-
+      <section className={style.segundaSection}>
+        <h2>Conheça nossa equipe de Design, Marketing e Desenvolvimento de Software.</h2>
+        <div>
+          
+        </div>
+      </section>
     </div>
-  )
+  );
 }
