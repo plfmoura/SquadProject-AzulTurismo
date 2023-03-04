290a5eef67e5cdccdf83caa76cbb6829dae33452
plfmoura <plfmoura96@gmail.com>
2023-03-03 23:21:32 -0300

    alter(singleSevice)styles/align/CSS inline

--- src/pages/Description/index.jsx
+++ src/pages/Description/index.jsx
@@ -3,17 +3,17 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import style from "./description.module.css";
 import { teamService } from "../../services/teamService";
-import { AiFillStar } from 'react-icons/ai';
-import { TfiMedallAlt } from 'react-icons/tfi';
-
+import { AiFillStar } from "react-icons/ai";
+import { TfiMedallAlt } from "react-icons/tfi";
+import GoogleMaps from "../../components/GoogleMaps";
 
 export default function Description() {
   let id = useParams();
   const [tour, setTour] = useState();
   const [auxiliary, setAuxiliary] = useState();
-  const [guide, setGuide] = useState()
-  const [photographer, setPhotographer] = useState()
-  const [index, setIndex] = useState(0)
+  const [guide, setGuide] = useState();
+  const [photographer, setPhotographer] = useState();
+  const [index, setIndex] = useState(0);
   const state = useSelector((state) => state);
   const { products } = state.shopping;
 
@@ -21,32 +21,34 @@ export default function Description() {
     let selected_id = Number(id.id.replace(":", ""));
     let selected_tour = products.find((product) => product.id === selected_id);
     setTour(selected_tour);
-    console.log(selected_tour);
-    setAuxiliary(teamService.auxiliary)
-    setGuide(teamService.guide)
-    setPhotographer(teamService.photographer)
+    setAuxiliary(teamService.auxiliary);
+    setGuide(teamService.guide);
+    setPhotographer(teamService.photographer);
   }, [products]);
 
   useEffect(() => {
-    let i = Math.floor(Math.random() * 3)
-    setIndex(i)
-  }, [tour])
-  
+    let i = Math.floor(Math.random() * 3);
+    setIndex(i);
+  }, [tour]);
+
   return (
     <div className={style.singleServiceContainer}>
       {tour && (
         <>
           <section className={style.serviceMedia}>
-            <img src={tour.imagens[0]} alt="" />
+            <img src={tour.imagens[0]} alt={tour.name} />
             <div>
-              <img src={tour.imagens[1]} alt="" />
-              <img src={tour.imagens[2]} alt="" />
-              <img src={tour.imagens[3]} alt="" />
+              <img src={tour.imagens[1]} alt={tour.name} />
+              <img src={tour.imagens[2]} alt={tour.name} />
+              <img src={tour.imagens[3]} alt={tour.name} />
             </div>
           </section>
           <section className={style.serviceDescription}>
             <div className={style.serviceOverView}>
-              <h1>{tour.name}</h1>
+              <div style={{display: 'flex', alignItems: 'center', fontSize: '24px'}}>
+                <h1>{tour.name}</h1>
+                <span style={{display: 'flex'}}><AiFillStar/> {tour.rating}</span>
+              </div>
               <p>{tour.located}</p>
               <div>
                 {tour.included.map((item, key) => (
@@ -64,60 +66,120 @@ export default function Description() {
               </div>
             </div>
           </section>
-      <section className={style.teamContainer}>
-        <h2>Nosso time em <span>{tour.name}</span></h2>
-        <div className={style.teamContent}>
-        { tour &&
-          <>
-          <div className={style.guideContainer}>
-            <img src={guide[index].picture} />
-            <div>
-              <p className={style.teamName}>Guia {guide[index].name}</p>
-              <div style={{display: 'flex'}} className={style.teamOverview}>
-                <p>{guide[index].role} desde {guide[index].firstContract}</p>
-                <p>Idiomas: {guide[index].languages.map(item => `${item}`)}</p>
-              </div>
+          <section className={style.teamContainer}>
+            <h2>
+              Nosso time em <span>{tour.name}</span>
+            </h2>
+            <div className={style.teamContent}>
+              {tour && (
+                <>
+                {/* area do lider da excursão, GUIA */}
+                  <div className={style.guideContainer}>
+                    <img src={guide[index].picture} />
+                    <div>
+                      <p className={style.teamName}>Guia {guide[index].name}</p>
+                      <div
+                        style={{ display: "flex" }}
+                        className={style.teamOverview}
+                      >
+                        <p>
+                          {guide[index].role} desde {guide[index].firstContract}
+                        </p>
+                        <p>
+                          Idiomas:{" "}
+                          {guide[index].languages.map((item) => `${item}`)}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={style.teamRating}>
+                    <p style={{ display: "flex", alignItems: "center" }}>
+                      <AiFillStar style={{ fontSize: "20px" }} />{" "}
+                      {guide[index].rate} Avaliações
+                    </p>
+                    <p style={{ display: "flex", alignItems: "center" }}>
+                      <TfiMedallAlt
+                        style={{ color: "#2e99ff", fontSize: "20px" }}
+                      />{" "}
+                      {guide[index].level}
+                    </p>
+                  </div>
+                  {/* Area do Auxiliar do guia */}
+                  <div className={style.auxContainer}>
+                    <img src={auxiliary[index].picture} />
+                    <div>
+                      <p className={style.teamName}>
+                        Aux. {auxiliary[index].name}
+                      </p>
+                      <div
+                        style={{ display: "flex" }}
+                        className={style.teamOverview}
+                      >
+                        <p>
+                          {auxiliary[index].role} desde{" "}
+                          {auxiliary[index].firstContract}
+                        </p>
+                        <p>
+                          Idiomas:{" "}
+                          {auxiliary[index].languages.map((item) => `${item}`)}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={style.teamRating}>
+                    <p style={{ display: "flex", alignItems: "center" }}>
+                      <AiFillStar style={{ fontSize: "20px" }} />{" "}
+                      {auxiliary[index].rate} Avaliações
+                    </p>
+                    <p style={{ display: "flex", alignItems: "center" }}>
+                      <TfiMedallAlt
+                        style={{ color: "aqua", fontSize: "20px" }}
+                      />{" "}
+                      {auxiliary[index].level}
+                    </p>
+                  </div>
+                {/* Area do fotografo */}
+                  <div className={style.photoContainer}>
+                    <img src={photographer[index].picture} />
+                    <div>
+                      <p className={style.teamName}>
+                        Fotógrafa {photographer[index].name}
+                      </p>
+                      <div
+                        style={{ display: "flex" }}
+                        className={style.teamOverview}
+                      >
+                        <p>
+                          {photographer[index].role} desde{" "}
+                          {photographer[index].firstContract}
+                        </p>
+                        <p>
+                          Idiomas:{" "}
+                          {photographer[index].languages.map(
+                            (item) => `${item}`
+                          )}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              )}
             </div>
-          </div>
-          <div className={style.teamRating}>
-            <p style={{display: 'flex', alignItems: 'center'}}><AiFillStar style={{fontSize: '20px'}}/> {guide[index].rate} Avaliações</p>
-            <p style={{display: 'flex', alignItems: 'center'}}><TfiMedallAlt style={{color: '#2e99ff', fontSize: '20px'}}/> {guide[index].level}</p>
-          </div>
-
-          <div className={style.auxContainer}>
-            <img src={auxiliary[index].picture} />
-            <div>
-              <p className={style.teamName}>Aux. {auxiliary[index].name}</p>
-              <div style={{display: 'flex'}} className={style.teamOverview}>
-                <p>{auxiliary[index].role} desde {auxiliary[index].firstContract}</p>
-                <p>Idiomas: {auxiliary[index].languages.map(item => `${item}`)}</p>
-              </div>
-            </div>
-          </div>
-          <div className={style.teamRating}>
-            <p style={{display: 'flex', alignItems: 'center'}}><AiFillStar style={{fontSize: '20px'}}/> {auxiliary[index].rate} Avaliações</p>
-            <p style={{display: 'flex', alignItems: 'center'}}><TfiMedallAlt style={{color: 'aqua', fontSize: '20px'}}/> {auxiliary[index].level}</p>
-          </div>
-
-          <div className={style.photoContainer}>
-            <img src={photographer[index].picture} />
-            <div>
-              <p className={style.teamName}>Fotógrafa {photographer[index].name}</p>
-              <div style={{display: 'flex'}} className={style.teamOverview}>
-                <p>{photographer[index].role} desde {photographer[index].firstContract}</p>
-                <p>Idiomas: {photographer[index].languages.map(item => `${item}`)}</p>
-              </div>
-            </div>
-          </div>
-          </>
-        }
-        </div>
-      </section>
-      </>
+          </section>
+        </>
       )}
-      <section className={style.mapsContainer}>
-        
-      </section>
+      {/* Google Maps  */}
+      {tour && (
+        <section className={style.mapsContainer}>
+          <h2>Nosso destino em {tour.name} através do Maps</h2>
+          <div>
+            <GoogleMaps
+              lat={Number(tour.latitude)}
+              long={Number(tour.longitude)}
+            />
+          </div>
+        </section>
+      )}
     </div>
   );
 }
