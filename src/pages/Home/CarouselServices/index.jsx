399d4744b6643c6d8bf80375aa7f2f388f66b1e1
plfmoura <plfmoura96@gmail.com>
2023-03-04 13:51:14 -0300

    alter(home)service slider adjust config

--- src/pages/Home/CarouselServices/index.jsx
+++ src/pages/Home/CarouselServices/index.jsx
@@ -1,28 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Slider from "react-slick";
-import { CiCircleChevLeft } from "react-icons/ci";
-
-function SampleNextArrow({ className, onClick }) {
-  return (
-    <CiCircleChevLeft
-      className={className}
-      style={{color: "#2eaaff", transform: 'scale(2.5) rotate(180deg)'}}
-      onClick={onClick}
-    />
-  );
-}
-
-function SamplePrevArrow({ className, onClick }) {
-  return (
-    <CiCircleChevLeft
-      className={className}
-      style={{color: "#2eaaff", transform: 'scale(2.5)'}}
-      onClick={onClick}
-    />
-  );
-}
+import { CiCircleChevLeft, CiCircleChevRight } from "react-icons/ci";
 
 export default function CarouselServices({ children, setClass }) {
+  const [hover, setHover] = useState(false);
+// area de estilo do btn do slider
+  const style = {
+    normal: {
+      color: '#333',
+      transform: 'scale(2.2) translateY(-1.5rem)',
+      marginInline: '-.5rem'
+    },
+
+    hover: {
+      color: '#2e44ff',
+    },
+  }
+
+  function SamplePrevArrow({ className, onClick }) {
+    return (
+      <CiCircleChevLeft
+        onMouseEnter={()=>{
+          setHover(true);
+        }}
+        onMouseLeave={()=>{
+          setHover(false);
+        }}
+        style={{
+          ...style.normal,
+          ...(hover ? style.hover : null)
+        }}
+        className={className}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function SampleNextArrow({ className, onClick }) {
+    return (
+      <CiCircleChevRight
+        className={className}
+        onMouseEnter={()=>{
+          setHover(true);
+        }}
+        onMouseLeave={()=>{
+          setHover(false);
+        }}
+        style={{
+          ...style.normal,
+          ...(hover ? style.hover : null)
+        }}
+        onClick={onClick}
+      />
+    );
+  }
+  
+//  Area de configurações do slider
   const settings = {
     className: "center",
     infinite: true,
