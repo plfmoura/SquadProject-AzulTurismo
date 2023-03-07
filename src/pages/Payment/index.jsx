cd47d6d80f4375da02c07ba2cd3094bf1a9d2537
WillianXG <wilian.mine789@gmail.com>
2023-03-07 16:22:41 -0300

    Card add in payment

--- src/pages/Payment/index.jsx
+++ src/pages/Payment/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Card from './Card'
 import style from './payment.module.css'
 
 export default function Payment() {
   return (
     <div className={style.paymentContainer}>
       <h1>Página de pagamento</h1>
+      <Card/>
     </div>
   )
 }
