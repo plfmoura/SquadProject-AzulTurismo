72213bd456ece79389207bd5e49630f6128c9098
Juanpi92 <juan.urgelles92@gmail.com>
2023-03-11 13:10:21 -0300

    compras

--- src/pages/Description/BuyForm/index.jsx
+++ src/pages/Description/BuyForm/index.jsx
@@ -1,43 +1,57 @@
-import React, { useContext } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import Button from "../../../components/Button";
-import { LoggedContext } from '../../../context/LoggedContext';
-import styles from './buyForm.module.css'
+import { LoggedContext } from "../../../context/LoggedContext";
+import styles from "./buyForm.module.css";
 
-export default function BuyForm({tourPrice, quantity, option, id}) {
-    const { hasUser, setHasUser, show, setShow  } = useContext(LoggedContext)
+export default function BuyForm({ tourPrice, quantity, option, id }) {
+  const state = useSelector((state) => state);
+  const { user } = state.user;
+  const { show, setShow } = useContext(LoggedContext);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log('Form concluído')
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form concluído");
+  };
 
-    return (
-    <form onSubmit={ handleSubmit } className={styles.formContainer}>
-        {/* RETIRAR GAMBIARRA PARA VERIFICAÇÃO SE TEM USUARIO */}
-        <h2 onClick={() => {setHasUser(!hasUser)}}>R${tourPrice}<span> /pessoa</span></h2>
-        <div>
-            <label htmlFor="date">Dia da Semana</label>
-            <select name="date" id="">
-                <option value="default"> Selecione </option>
-                { option }
-            </select>
-            <hr style={{border: '1px solid #33333322', width: '100%', margin: '1rem 0'}}/>
-            <label htmlFor="date">Quantidade de Pessoas</label>
-            <select name="quantity" id="">
-                <option value="default">{quantity}</option>
-            </select>
-        </div>
-        {hasUser ? ( 
-            <Button type='submit' text='Solicitar Compra' onPress={() => {
-                navigate(`/payment/:${id}`);}}/>
-                ) : ( 
-            <Button type='submit' text='Solicitar Compra' onPress={() => {
-                setShow(!show)}}/>
-                )
-        }
+  return (
+    <form onSubmit={handleSubmit} className={styles.formContainer}>
+      {/* RETIRAR GAMBIARRA PARA VERIFICAÇÃO SE TEM USUARIO */}
+      <h2>
+        R${tourPrice}
+        <span> /pessoa</span>
+      </h2>
+      <div>
+        <label htmlFor="date">Dia da Semana</label>
+        <select name="date" id="">
+          <option value="default"> Selecione </option>
+          {option}
+        </select>
+        <hr
+          style={{
+            border: "1px solid #33333322",
+            width: "100%",
+            margin: "1rem 0",
+          }}
+        />
+        <label htmlFor="date">Quantidade de Pessoas</label>
+        <select name="quantity" id="">
+          <option value="default">{quantity}</option>
+        </select>
+      </div>
+
+      <Button
+        type="submit"
+        text="Solicitar Compra"
+        onPress={() => {
+          {
+            user ? navigate(`/payment/:${id}`) : setShow(!show);
+          }
+        }}
+      />
     </form>
-  )
+  );
 }
