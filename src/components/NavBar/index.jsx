c167363624fe9b4be170683c86cd45423266b987
Juanpi92 <juan.urgelles92@gmail.com>
2023-03-11 00:46:44 -0300

    register and login

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,58 +1,101 @@
-import React, { useContext, useState } from 'react'
-import { Link } from 'react-router-dom'
-import style from './navBar.module.css'
-import { IoNotifications } from "react-icons/io5"
-import ProfilePicture from '../../assets/profile/user.jpg'
-import Modal from '../Modal'
-import SingIn from '../../pages/SingIn'
-import { LoggedContext } from '../../context/LoggedContext'
-import BurgerMobileMenu from './BurgerMobileMenu'
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import style from "./navBar.module.css";
+import { IoNotifications } from "react-icons/io5";
+import ProfilePicture from "../../assets/profile/user.jpg";
+import Modal from "../Modal";
+import SingIn from "../../pages/SingIn";
+import { LoggedContext } from "../../context/LoggedContext";
+import BurgerMobileMenu from "./BurgerMobileMenu";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { delUser } from "../../reducer/userReducer";
 
 export default function NavBar() {
-  const [ signed, setSigned ] = useState(true)
-  const { show, setShow } = useContext(LoggedContext)
+  const state = useSelector((state) => state);
+  const { user } = state.user;
+  const [signed, setSigned] = useState(true);
+  const { show, setShow } = useContext(LoggedContext);
+  const dispatch = useDispatch();
 
   return (
     <>
-    {show && <Modal setShow={setShow} children={ <SingIn />}/>}
+      {show && (
+        <Modal setShow={setShow} children={<SingIn setShow={setShow} />} />
+      )}
 
-    <div className={style.alignContent}>
-      <div className={style.navBarContainer}>
-        <div className={style.logoAlign}>
-          <img src="azul.png" alt="Logo da empresa Azul Turismo" className={style.navBarLogo} />
-          <span>AZUL TOUR</span>
-        </div>
-        <nav className={style.menuContainer}>
-          <ul className={style.menuContent}>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/'>Serviços</Link></li>
-            <li><Link to='/about'>Sobre nós</Link></li>
-          </ul>
-        </nav>
-        <div className={style.userContainer}>
-          <ul className={style.menuContent}>
-            {signed ? (
-              <>
-                <li><Link to='/profile'>Meus Passeios</Link></li>
-                <li onClick={() => setSigned(!signed)}><Link to='/'>Sair</Link></li>
-                <div>
-                  <Link to='/profile'><IoNotifications className={style.notificationIcon}/></Link>
-                  <Link to='/profile'><img src={ProfilePicture} alt="Imagem de Perfil do usuário" className={style.userPicture} /></Link>
-                </div>
-              </>) : (
+      <div className={style.alignContent}>
+        <div className={style.navBarContainer}>
+          <div className={style.logoAlign}>
+            <img
+              src="azul.png"
+              alt="Logo da empresa Azul Turismo"
+              className={style.navBarLogo}
+            />
+            <span>AZUL TOUR</span>
+          </div>
+          <nav className={style.menuContainer}>
+            <ul className={style.menuContent}>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/">Serviços</Link>
+              </li>
+              <li>
+                <Link to="/about">Sobre nós</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className={style.userContainer}>
+            <ul className={style.menuContent}>
+              {user ? (
                 <>
-                  <li onClick={() => setShow(true)}><Link>Entrar</Link></li>
+                  <li>
+                    <Link to="/profile">Meus Passeios</Link>
+                  </li>
+                  <li
+                    onClick={() => {
+                      dispatch(delUser());
+                      localStorage.removeItem("token");
+                    }}
+                  >
+                    <Link to="/">Sair</Link>
+                  </li>
                   <div>
-                    <Link to='/profile'><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVKTCvhbnqwyIbeN8eZAzlzb9s9d6LBnNWsw&usqp=CAU' alt="Imagem de Perfil do usuário" className={style.userPicture} /></Link>
+                    <Link to="/profile">
+                      <IoNotifications className={style.notificationIcon} />
+                    </Link>
+                    <Link to="/profile">
+                      <img
+                        src={ProfilePicture}
+                        alt="Imagem de Perfil do usuário"
+                        className={style.userPicture}
+                      />
+                    </Link>
                   </div>
                 </>
-              )
-            }
-          </ul>
+              ) : (
+                <>
+                  <li onClick={() => setShow(true)}>
+                    <Link>Entrar</Link>
+                  </li>
+                  <div>
+                    <Link to="/profile">
+                      <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVKTCvhbnqwyIbeN8eZAzlzb9s9d6LBnNWsw&usqp=CAU"
+                        alt="Imagem de Perfil do usuário"
+                        className={style.userPicture}
+                      />
+                    </Link>
+                  </div>
+                </>
+              )}
+            </ul>
+          </div>
+          <BurgerMobileMenu callMenu={style.menuMobile} />
         </div>
-        <BurgerMobileMenu callMenu={style.menuMobile}/>
       </div>
-    </div>
     </>
-  )
+  );
 }
--- src/pages/SingIn/index.jsx
+++ src/pages/SingIn/index.jsx
@@ -1,57 +1,164 @@
-import React, { useState } from 'react'
-import Button from '../../components/Button'
-import FacebookAuth from './FacebookAuth'
-import GoogleAuth from './GoogleAuth'
-import style from './singIn.module.css'
+import React, { useRef, useState } from "react";
+import Button from "../../components/Button";
+import FacebookAuth from "./FacebookAuth";
+import GoogleAuth from "./GoogleAuth";
+import style from "./singIn.module.css";
+import axios, { AxiosHeaders } from "axios";
+import { json } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../reducer/userReducer";
 
-export default function SingIn() {
-    const [ login, setLogin ] = useState(false)
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log('Usuário cadastrado com sucesso!')
+export default function SingIn({ setShow }) {
+  const [login, setLogin] = useState(false);
+  const loginForm = useRef();
+  const registerForm = useRef();
+  const dispatch = useDispatch();
+  // Function Register
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    let name = registerForm.current.name.value;
+    let email = registerForm.current.email.value;
+    let password = registerForm.current.password.value;
+    let confirmPassword = registerForm.current.confirmPassword.value;
+    if (password != confirmPassword) {
+      alert("As senhas não coinciden");
+      return;
     }
+
+    const options = {
+      method: "POST",
+      /*url: "https://tourismapi.herokuapp.com/register",*/
+      url: "http://127.0.0.1:3000/register",
+      headers: { "Content-Type": "application/json" },
+      data: {
+        name: `${name}`,
+        email: `${email}`,
+        password: `${password}`,
+        idioms: "",
+        profession: "",
+        located: "",
+        tel1: "",
+        tel2: "",
+        about: "",
+        images: ["", "", "", ""],
+        image_banner: "",
+        image_profile: "",
+      },
+    };
+    try {
+      await axios.request(options);
+      alert("Cadastro Satifatorio");
+      loginForm.current.reset();
+    } catch (error) {
+      alert("Usuario ja cadastrado");
+      loginForm.current.reset();
+    }
+  };
+  //function login
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    let email = loginForm.current.email_login.value;
+    let password = loginForm.current.password_login.value;
+
+    const options = {
+      method: "POST",
+      url: "http://127.0.0.1:3000/login",
+      headers: { "Content-Type": "application/json" },
+      data: {
+        email: `${email}`,
+        password: `${password}`,
+      },
+    };
+    try {
+      let response = await axios.request(options);
+      dispatch(setUser(response.data.user));
+      localStorage.setItem("token", JSON.stringify(response.data.token));
+      loginForm.current.reset();
+      setShow(false);
+    } catch (error) {
+      alert("Usuario ou senha errada");
+    }
+  };
+
   return (
     <div>
-      {login ? (<form onSubmit={ handleSubmit } className={style.singInContainer}>
-        <div className={style.formHeader}>
+      {login ? (
+        <form className={style.singInContainer} ref={registerForm}>
+          <div className={style.formHeader}>
             <img src="azul.png" alt="Logo da Empresa Azul Turismo" />
             <span>Cadastre-se agora</span>
             <p>Faça seu cadastro agora e comece a explorar nossos passeios.</p>
-        </div>
-        <div className={style.formMain}>
-            <input type="text" placeholder='Insira seu nome'/>            
-            <input type="email" placeholder='Insira seu email'/>            
-            <input type="password" placeholder='Insira uma senha'/>            
-            <input type="password" placeholder='Confirme sua senha'/>            
-        </div>
-        <div className={style.formFooter}>
-            <Button onPress={ handleSubmit } text='Cadastrar'/>
+          </div>
+          <div className={style.formMain}>
+            <input
+              type="text"
+              placeholder="Insira seu nome"
+              id="name"
+              required
+            />
+            <input
+              type="email"
+              placeholder="Insira seu email"
+              pattern="^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$"
+              id="email"
+              required
+            />
+            <input
+              type="password"
+              placeholder="Insira uma senha"
+              id="password"
+              required
+            />
+            <input
+              type="password"
+              placeholder="Confirme sua senha"
+              id="confirmPassword"
+              required
+            />
+          </div>
+          <div className={style.formFooter}>
+            <Button onPress={handleRegister} text="Cadastrar" />
             <span>ou</span>
             <FacebookAuth />
             <GoogleAuth />
-            <span onClick={() => setLogin(!login)}>Já tem uma conta? <strong>Entrar</strong></span>
-        </div>
-      </form>) : (
-        <form onSubmit={ handleSubmit } className={style.loginContainer}>
-        <div className={style.formHeader}>
+            <span onClick={() => setLogin(!login)}>
+              Já tem uma conta? <strong>Entrar</strong>
+            </span>
+          </div>
+        </form>
+      ) : (
+        <form className={style.loginContainer} ref={loginForm}>
+          <div className={style.formHeader}>
             <img src="azul.png" alt="Logo da Empresa Azul Turismo" />
             <span>Entre como usuário</span>
             <p>Entre agora e comece a explorar nossos passeios.</p>
-        </div>
-        <div className={style.formMain}>
-            <input type="email" placeholder='Email cadastrado'/>            
-            <input type="password" placeholder='Senha de usuário'/>            
-        </div>
-        <div className={style.formFooter}>
-            <Button onPress={ handleSubmit } text='Entrar como Usuário'/>
+          </div>
+          <div className={style.formMain}>
+            <input
+              type="email"
+              placeholder="Email cadastrado"
+              pattern="^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$"
+              required
+              id="email_login"
+            />
+            <input
+              type="password"
+              placeholder="Senha de usuário"
+              id="password_login"
+              required
+            />
+          </div>
+          <div className={style.formFooter}>
+            <Button onPress={handleLogin} text="Entrar como Usuário" />
             <span>ou</span>
-            <FacebookAuth />
-            <GoogleAuth />
-            <span onClick={() => setLogin(!login)}>Não é cadastrado? <strong>Cadastre-se</strong></span>
-        </div>
-      </form>
+            {/*  <FacebookAuth />
+            <GoogleAuth />*/}
+            <span onClick={() => setLogin(!login)}>
+              Não é cadastrado? <strong>Cadastre-se</strong>
+            </span>
+          </div>
+        </form>
       )}
     </div>
-  )
+  );
 }
