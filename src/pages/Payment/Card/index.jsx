f878973c18f51d2d66fd31f8ab445b8b0853df36
WillianXG <wilian.mine789@gmail.com>
2023-03-23 11:14:54 -0300

    bug teste

--- src/pages/Payment/Card/index.jsx
+++ src/pages/Payment/Card/index.jsx
@@ -51,93 +51,7 @@ export default function Card({ handleCheckout, purchaseReturn }) {
 
   return (
     <>
-    {!onSuccess ? (
-    <div className={styles.cardContainer} style={{maxHeight: showArea}}>
-      <div className={styles.cardPayment_header}>
-        <h4 onClick={handleChange} style={{cursor: 'pointer', marginLeft: "2.5%"}}>Pagamento com Cartão</h4>
-        <CreditCardPayment />
-      </div>
-      <div className={styles.cardPaymentMethod_container} style={{display: hideContent}}>
-        <Cards
-          cvc={cardDetails.cvc}
-          expiry={cardDetails.expiry}
-          focused={cardDetails.focus}
-          name={cardDetails.name}
-          number={cardDetails.number}
-          cpf={cardDetails.cpf}
-          placeholders={{
-            name: "SEU NOME AQUI",
-          }}
-        />
-        <div>
-          <form className={styles.cardForm} onSubmit={handleCheckout}>
-            <InputElement
-              mask='9999 9999 9999 9999'
-              type="tel"
-              name="number"
-              placeholder="0000 0000 0000 0000"
-              onChange={handleInputChange}
-              onFocus={handleInputFocus}
-              value={cardDetails.number}
-              pattern="^[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}$"
-              required
-            />
-            <div className={styles.mainContent}>
-              <InputElement
-                type="text"
-                name="name"
-                placeholder="Titular do Cartão"
-                onChange={handleInputChange}
-                onFocus={handleInputFocus}
-                value={cardDetails.name}
-                required
-              />
-              <InputElement
-                type="text"
-                name="expiry"
-                mask='99/99'
-                placeholder="MM/AA"
-                onChange={handleInputChange}
-                onFocus={handleInputFocus}
-                value={cardDetails.expiry}
-                required
-              />
-            </div>
-            <div className={styles.bottom}>
-              <InputElement
-                type="tel"
-                name="CPF"
-                mask='999.999.999-99'
-                placeholder="CPF DO TITULAR"
-                pattern="^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}$"
-                required
-              />
-              <input
-                type="tel"
-                name="cvc"
-                mask='999'
-                placeholder="CVC"
-                onChange={handleInputChange}
-                onFocus={handleInputFocus}
-                value={cardDetails.cvc}
-                pattern="^[0-9]{3}$"
-                maxLength={3}
-                required
-              />
-            </div>
-            <div className={styles.submitArea}>
-              <Button text={"Confirmar Compra"} type="submit" />
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-    ) : (
-      <div className={styles.onPurchaseSuccess}>
-        <OnSuccessAnimation />
-      </div>
-    )
-    }
+
       {!onSuccess ? (
         <div className={styles.cardContainer}>
           <Cards
@@ -151,7 +65,6 @@ export default function Card({ handleCheckout, purchaseReturn }) {
               name: "SEU NOME AQUI",
             }}
           />
-
           <div className={styles.cardFlagsContainer}>
             <h3 className={styles.cardFlagsTitle}>Bandeiras aceitas</h3>
             <div className={styles.cardsFlags}>
