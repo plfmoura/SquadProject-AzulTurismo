93166442ff9ded63ce47bbe8b16a166ae54f853c
WillianXG <wilian.mine789@gmail.com>
2023-03-23 11:30:11 -0300

    bug resolve

--- src/pages/Payment/Card/index.jsx
+++ src/pages/Payment/Card/index.jsx
@@ -40,10 +40,10 @@ export default function Card({ handleCheckout, purchaseReturn }) {
     setCardDetails({ ...cardDetails, [name]: value });
   };
 
-  const [ show, setShow ] = useState(false)
-  
+  const [show, setShow] = useState(false)
+
   let showArea = show ? '100%' : '3rem';
-  let hideContent = show? 'block' : 'none';
+  let hideContent = show ? 'block' : 'none';
 
   const handleChange = () => {
     setShow(!show)
@@ -51,93 +51,99 @@ export default function Card({ handleCheckout, purchaseReturn }) {
 
   return (
     <>
-
       {!onSuccess ? (
-        <div className={styles.cardContainer}>
-          <Cards
-            cvc={cardDetails.cvc}
-            expiry={cardDetails.expiry}
-            focused={cardDetails.focus}
-            name={cardDetails.name}
-            number={cardDetails.number}
-            cpf={cardDetails.cpf}
-            placeholders={{
-              name: "SEU NOME AQUI",
-            }}
-          />
-          <div className={styles.cardFlagsContainer}>
-            <h3 className={styles.cardFlagsTitle}>Bandeiras aceitas</h3>
-            <div className={styles.cardsFlags}>
-              <FaCcVisa size={55} color={"#191970"} style={{ marginRight: '10px' }} />
-              <FaCcMastercard size={55} color={"#ff4600"} style={{ marginRight: '10px' }} />
-              <FaCcDiscover size={55} color={"#c0c0c0"} style={{ marginRight: '10px' }} />
-              <FaCcAmex size={55} color={"#191970"} style={{ marginRight: '10px' }} />
-              <FaCcJcb size={55} color={"#f00000"} style={{ marginRight: '10px' }} />
-              <FaCcDinersClub color={"#a2d1e9"} size={55} />
-            </div>
+        <div className={styles.cardContainer} style={{ maxHeight: showArea }}>
+          <div className={styles.cardPayment_header}>
+            <h4 onClick={handleChange} style={{ cursor: 'pointer', marginLeft: "2.5%" }}>Pagamento com Cartão</h4>
+            <CreditCardPayment />
           </div>
-          <div>
+          <div className={styles.cardPaymentMethod_container} style={{ display: hideContent }}>
+            <Cards
+              cvc={cardDetails.cvc}
+              expiry={cardDetails.expiry}
+              focused={cardDetails.focus}
+              name={cardDetails.name}
+              number={cardDetails.number}
+              cpf={cardDetails.cpf}
+              placeholders={{
+                name: "SEU NOME AQUI",
+              }}
+            />
+
+            <div className={styles.cardFlagsContainer}>
+              <h4 className={styles.cardFlagsTitle}>Bandeiras aceitas</h4>
+              <div className={styles.cardsFlags}>
+                <FaCcVisa size={55} color={"#191970"} style={{ marginRight: '10px', marginLeft: '35px' }} />
+                <FaCcMastercard size={55} color={"#ff4600"} style={{ marginRight: '10px' }} />
+                <FaCcDiscover size={55} color={"#c0c0c0"} style={{ marginRight: '10px' }} />
+                <FaCcAmex size={55} color={"#191970"} style={{ marginRight: '10px' }} />
+                <FaCcJcb size={55} color={"#f00000"} style={{ marginRight: '10px' }} />
+                <FaCcDinersClub color={"#a2d1e9"} size={55} />
+              </div>
+            </div>
 
 
-            <form className={styles.cardForm} onSubmit={handleCheckout}>
-              <InputElement
-                mask='9999 9999 9999 9999'
-                type="tel"
-                name="number"
-                placeholder="0000 0000 0000 0000"
-                onChange={handleInputChange}
-                onFocus={handleInputFocus}
-                value={cardDetails.number}
-                pattern="^[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}$"
-                required
-              />
-              <div className={styles.mainContent}>
+            <div>
+              <form className={styles.cardForm} onSubmit={handleCheckout}>
                 <InputElement
-                  type="text"
-                  name="name"
-                  placeholder="Titular do Cartão"
-                  onChange={handleInputChange}
-                  onFocus={handleInputFocus}
-                  value={cardDetails.name}
-                  required
-                />
-                <InputElement
-                  type="text"
-                  name="expiry"
-                  mask='99/99'
-                  placeholder="MM/AA"
-                  onChange={handleInputChange}
-                  onFocus={handleInputFocus}
-                  value={cardDetails.expiry}
-                  required
-                />
-              </div>
-              <div className={styles.bottom}>
-                <InputElement
+                  mask='9999 9999 9999 9999'
                   type="tel"
-                  name="CPF"
-                  mask='999.999.999-99'
-                  placeholder="CPF DO TITULAR"
-                  pattern="^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}$"
-                  required
-                />
-                <input
-                  type="tel"
-                  name="cvc"
-                  mask='999'
-                  placeholder="CVC"
+                  name="number"
+                  placeholder="0000 0000 0000 0000"
                   onChange={handleInputChange}
                   onFocus={handleInputFocus}
-                  value={cardDetails.cvc}
-                  pattern="^[0-9]{3}$"
-                  maxLength={3}
+                  value={cardDetails.number}
+                  pattern="^[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}$"
                   required
                 />
-              </div>
-              <div className={styles.submitArea}>
-                <Button text={"Confirmar Compra"} type="submit" />
-              </div>
-            </form>
+                <div className={styles.mainContent}>
+                  <InputElement
+                    type="text"
+                    name="name"
+                    placeholder="Titular do Cartão"
+                    onChange={handleInputChange}
+                    onFocus={handleInputFocus}
+                    value={cardDetails.name}
+                    required
+                  />
+                  <InputElement
+                    type="text"
+                    name="expiry"
+                    mask='99/99'
+                    placeholder="MM/AA"
+                    onChange={handleInputChange}
+                    onFocus={handleInputFocus}
+                    value={cardDetails.expiry}
+                    required
+                  />
+                </div>
+                <div className={styles.bottom}>
+                  <InputElement
+                    type="tel"
+                    name="CPF"
+                    mask='999.999.999-99'
+                    placeholder="CPF DO TITULAR"
+                    pattern="^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}$"
+                    required
+                  />
+                  <input
+                    type="tel"
+                    name="cvc"
+                    mask='999'
+                    placeholder="CVC"
+                    onChange={handleInputChange}
+                    onFocus={handleInputFocus}
+                    value={cardDetails.cvc}
+                    pattern="^[0-9]{3}$"
+                    maxLength={3}
+                    required
+                  />
+                </div>
+                <div className={styles.submitArea}>
+                  <Button text={"Confirmar Compra"} type="submit" />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       ) : (
@@ -146,6 +152,7 @@ export default function Card({ handleCheckout, purchaseReturn }) {
         </div>
       )
       }
+
     </>
   );
 }
