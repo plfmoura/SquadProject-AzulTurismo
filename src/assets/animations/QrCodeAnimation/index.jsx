273a9460dc568c925a55b9fbfb9b7e8c67f1cef0
plfmoura <plfmoura96@gmail.com>
2023-03-24 14:16:59 -0300

    alter(payment): min animations time/loop

--- src/assets/animations/QrCodeAnimation/index.jsx
+++ src/assets/animations/QrCodeAnimation/index.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Lottie from 'react-lottie-player'
 import qrCode from './qr-code.json'
 
 export default function QrCodeAnimation() {
+    const [ play, setPlay ] = useState(true)
+
+    !play ? (
+        setTimeout(() => {
+            setPlay(true)
+        }, [7000])) : (null)
 
     return (
         <Lottie 
-            className='testando'
-            loop={true}
             animationData={qrCode}
-            play={true}
+            play={play}
             speed={0.6}
             style={{ width: 60, height: 60}}
+            onLoopComplete={() => setPlay(false)}
         />
     )
 }
