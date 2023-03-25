b6dd437a3e521a34cb221f57791b03e8b315f882
Vitória Moraes <arteslima135@gmail.com>
2023-03-25 17:58:05 -0300

    Implementando titulo,input e fazendo a validaçao

--- src/pages/Faq/index.jsx
+++ src/pages/Faq/index.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
-import style from "./FAQ.css";
+import React, { useState } from 'react'
+import Axios from "axios"
+import  "./FAQ.css";
+import { IoSearch } from 'react-icons/io5'
 const index = () => {
+
+  // OBJETO TESTE
+
+ 
+  // STATE DO MEU INPUT
+  const [DataInput,setDataInput] = useState({
+    data:"",
+  })
+
+  // FERIFICAÇAO DO VALOR DO MEU INPUT
+  const HandleChange =(e) =>{
+
+    setDataInput((prev)=>({...prev,[e.target.name]: e.target.value}))
+
+  }
+
+  //  FUNÇAO DE CLICK
+  const BtnSearch = () =>{
+    if (DataInput.data == "") {
+     alert("digite algo")
+    }
+     else {console.log(DataInput)}
+  }
+
+ 
   return (
     <div className='FAQ'>
         <section className='InfosFaq'>
             <h1>Ola! Precisando de Ajuda?</h1>
             <div className="inputSearch">
-                <input type="text" />
-                <div className="Search">
-                    <img src="" alt="" />
-                </div>
+
+                <input name="data" type="text" placeholder='Deixe aqui sua duvida...' onChange={HandleChange} />
+                <button type='submit' onClick={BtnSearch}><IoSearch /></button>
+              
             </div>
         </section>
         <section className='BtnFaq'></section>
