fabe707cc9528c83d72d0175639e8075fe278d0f
Guilherme Felipe Dos Santos <FELLIPEGUILHERME37@GMAIL.COM>
2023-03-27 20:09:06 -0300

    Faq - aplicação de estilo
    aplicações de estilos nos botões, endentação de código no css, pagina responsiva completamente

--- src/pages/Faq/index.jsx
+++ src/pages/Faq/index.jsx
@@ -1,16 +1,18 @@
-import React, { useContext, useEffect, useState } from 'react'
-import style from './FAQ.module.css'
-import { IoSearch } from 'react-icons/io5'
-import OffCanvas from './OffCanvas';
-import { NavBarContext } from '../../context/NavBarContext';
-import axios from "axios"
-import PreLoader from '../../components/PreLoader'
-import { dataFaq } from '../../services/faq';
+import React, { useContext, useEffect, useState } from "react";
+import style from "./FAQ.module.css";
+import { IoSearch } from "react-icons/io5";
+import OffCanvas from "./OffCanvas";
+import { NavBarContext } from "../../context/NavBarContext";
+import axios from "axios";
+import PreLoader from "../../components/PreLoader";
+import { dataFaq } from "../../services/faq";
+import { FaUserCog, FaRegCheckSquare, FaRegCreditCard, FaRegComments } from "react-icons/fa";
 
 const index = () => {
   // OVERLAY
-  const { setBgColor, setPaymentFooter, showOffCanvas, setShowOffCanvas } = useContext(NavBarContext);
-  const [ getData, setGetData ] = useState(dataFaq)
+  const { setBgColor, setPaymentFooter, showOffCanvas, setShowOffCanvas } =
+    useContext(NavBarContext);
+  const [getData, setGetData] = useState(dataFaq);
 
   useEffect(() => {
     // para subir a ao topo após renderizar a página
@@ -22,8 +24,8 @@ const index = () => {
   }, []);
 
   // STATE DOS MEUS BUTTONS
-  const [DataBtn,setDataBtn] = useState()
-  
+  const [DataBtn, setDataBtn] = useState();
+
   // REQUISIÇAO A API
   //  const ConsultaApi = (URL) =>{
   //   axios.get(`http://localhost:3000/${URL}`)
@@ -37,178 +39,184 @@ const index = () => {
 
   //  FUNÇAO DE CLICK do search
   const BtnSearch = () => {
-    const verificar = DataInput.data
+    const verificar = DataInput.data;
     if (verificar == "") {
-      alert("digite algo")
+      alert("digite algo");
+    } else if (verificar.includes("usuario") == true) {
+      console.log("fazendo a requisiçao a tabela de usuario");
+      setShowOffCanvas(true);
+      ConsultaApi("Usuario");
+    } else if (verificar.includes("Usuario") == true) {
+      console.log("fazendo a requisiçao a tabela de usuario");
+      setShowOffCanvas(true);
+      ConsultaApi("Usuario");
+    } else if (verificar.includes("usuarios") == true) {
+      console.log("fazendo a requisiçao a tabela de usuario");
+      setShowOffCanvas(true);
+      ConsultaApi("Usuario");
+    } else if (verificar.includes("Usuarios") == true) {
+      console.log("fazendo a requisiçao a tabela de usuario");
+      setShowOffCanvas(true);
+      ConsultaApi("Usuario");
+    } else if (verificar.includes("pagamentos") == true) {
+      console.log("fazendo a req a tabela pagamento");
+      setShowOffCanvas(true);
+      ConsultaApi("Pagamento");
+    } else if (verificar.includes("Pagamentos") == true) {
+      console.log("fazendo a req a tabela pagamento");
+      setShowOffCanvas(true);
+      ConsultaApi("Pagamento");
+    } else if (verificar.includes("pagamento") == true) {
+      console.log("fazendo a req a tabela pagamento");
+      setShowOffCanvas(true);
+      ConsultaApi("Pagamento");
+    } else if (verificar.includes("Pagamento") == true) {
+      console.log("fazendo a req a tabela pagamento");
+      setShowOffCanvas(true);
+      ConsultaApi("Pagamento");
+    } else if (verificar.includes("Garantias") == true) {
+      console.log("fazendo a req a tabela Garantias e segurança");
+      setShowOffCanvas(true);
+      ConsultaApi("seguranca");
+    } else if (verificar.includes("garantias") == true) {
+      console.log("fazendo a req a tabela Garantias e segurança");
+      setShowOffCanvas(true);
+      ConsultaApi("seguranca");
+    } else if (verificar.includes("Garantia") == true) {
+      console.log("fazendo a req a tabela Garantias e segurança");
+      setShowOffCanvas(true);
+      ConsultaApi("seguranca");
+    } else if (verificar.includes("garantia") == true) {
+      console.log("fazendo a req a tabela Garantias e segurança");
+      setShowOffCanvas(true);
+      ConsultaApi("seguranca");
+    } else if (verificar.includes("Segurança") == true) {
+      console.log("fazendo a req a tabela Garantias e segurança");
+      setShowOffCanvas(true);
+      ConsultaApi("seguranca");
+    } else if (verificar.includes("segurança") == true) {
+      console.log("fazendo a req a tabela Garantias e segurança");
+      setShowOffCanvas(true);
+      ConsultaApi("seguranca");
+    } else {
+      console.log("Nao sou capaz de responder sua pergunta!");
     }
-
-    else if (verificar.includes("usuario") == true) {
-      console.log("fazendo a requisiçao a tabela de usuario")
-      setShowOffCanvas(true) 
-      ConsultaApi("Usuario")
-    }
-    else if (verificar.includes("Usuario") == true) {
-      console.log("fazendo a requisiçao a tabela de usuario")
-      setShowOffCanvas(true)
-      ConsultaApi("Usuario")
-    }
-    else if (verificar.includes("usuarios") == true) {
-      console.log("fazendo a requisiçao a tabela de usuario")
-      setShowOffCanvas(true)
-      ConsultaApi("Usuario")
-    }
-    else if (verificar.includes("Usuarios") == true) {
-      console.log("fazendo a requisiçao a tabela de usuario")
-      setShowOffCanvas(true)
-      ConsultaApi("Usuario")
-    }
-    else if (verificar.includes("pagamentos") == true) {
-      console.log("fazendo a req a tabela pagamento")
-      setShowOffCanvas(true)
-      ConsultaApi("Pagamento")
-    }
-    else if (verificar.includes("Pagamentos") == true) {
-      console.log("fazendo a req a tabela pagamento")
-      setShowOffCanvas(true)
-      ConsultaApi("Pagamento")
-    }
-    else if (verificar.includes("pagamento") == true) {
-      console.log("fazendo a req a tabela pagamento")
-      setShowOffCanvas(true)
-      ConsultaApi("Pagamento")
-    }
-    else if (verificar.includes("Pagamento") == true) {
-      console.log("fazendo a req a tabela pagamento")
-      setShowOffCanvas(true)
-      ConsultaApi("Pagamento")
-    }
-    else if (verificar.includes("Garantias") == true) {
-      console.log("fazendo a req a tabela Garantias e segurança")
-      setShowOffCanvas(true)
-      ConsultaApi("seguranca")
-    }
-    else if (verificar.includes("garantias") == true) {
-      console.log("fazendo a req a tabela Garantias e segurança")
-      setShowOffCanvas(true)
-      ConsultaApi("seguranca")
-    }
-    else if (verificar.includes("Garantia") == true) {
-      console.log("fazendo a req a tabela Garantias e segurança")
-      setShowOffCanvas(true)
-      ConsultaApi("seguranca")
-    }
-    else if (verificar.includes("garantia") == true) {
-      console.log("fazendo a req a tabela Garantias e segurança")
-      setShowOffCanvas(true)
-      ConsultaApi("seguranca")
-    }
-    else if (verificar.includes("Segurança") == true) {
-      console.log("fazendo a req a tabela Garantias e segurança")
-      setShowOffCanvas(true)
-      ConsultaApi("seguranca")
-    }
-    else if (verificar.includes("segurança") == true) {
-      console.log("fazendo a req a tabela Garantias e segurança")
-      setShowOffCanvas(true)
-      ConsultaApi("seguranca")
-    }
-
-    else {
-      console.log("Nao sou capaz de responder sua pergunta!")
-    }
-
-
-
-  }
-
+  };
   // STATE DO MEU INPUT
   const [DataInput, setDataInput] = useState({
     data: "",
-  })
-
+  });
   // FERIFICAÇAO DO VALOR DO MEU INPUT
   const HandleChange = (e) => {
-
-    setDataInput((prev) => ({ ...prev, [e.target.name]: e.target.value }))
-  }
-
-  const [ dataFind, setDataFind ] = useState()
-  const [ valueBtn, setValueBtn ] = useState('') 
-  const [ loading, setLoading ] = useState(true)
+    setDataInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const [dataFind, setDataFind] = useState();
+  const [valueBtn, setValueBtn] = useState("");
+  const [loading, setLoading] = useState(true);
 
   // FUNCTION CLICK BTN
-  const handleSelect = (e) =>{
+  const handleSelect = (e) => {
     // colocando valor do btn para a variavel
-    let getBtnValue = e.target.value
-    setValueBtn(getBtnValue)
+    let getBtnValue = e.target.value;
+    setValueBtn(getBtnValue);
     // carregamento até mostrar a pesquisa
     setTimeout(() => {
-      setLoading(false)
-    }, [2000])
-    setShowOffCanvas(true)
+      setLoading(false);
+    }, [2000]);
+    setShowOffCanvas(true);
     // consulta o valor do target
     switch (getBtnValue) {
-      case 'usuario':
+      case "usuario":
         setDataBtn(getData.user);
         break;
-      case 'pagamento':
+      case "pagamento":
         setDataBtn(getData.payment);
         break;
-      case 'seguranca':
+      case "seguranca":
         setDataBtn(getData.security);
         break;
-      default: null;
+      default:
+        null;
     }
-  }
+  };
 
   useEffect(() => {
-    if(DataBtn){
-      setDataFind(DataBtn.find((value) => value.title.toLowerCase() === valueBtn))
+    if (DataBtn) {
+      setDataFind(
+        DataBtn.find((value) => value.title.toLowerCase() === valueBtn)
+      );
     }
-  }, [ DataBtn ])
-  
+  }, [DataBtn]);
+
   return (
     <>
-      {showOffCanvas &&
-        <OffCanvas children=
-          {!loading ?
-            (<div>
-             { dataFind && (
+      {showOffCanvas && (
+        <OffCanvas
+          children={
+            !loading ? (
+              <div>
+                {dataFind && (
                   <div>
-                    <h1 style={{color: '#f00'}}>{dataFind.title}</h1>
+                    <h1 style={{ color: "#f00" }}>{dataFind.title}</h1>
                   </div>
-             )
-             }
-             { DataBtn && (
-              DataBtn.map((item, key) => (
-              <div key={key}>
-                <h1>{item.title}</h1>
+                )}
+                {DataBtn &&
+                  DataBtn.map((item, key) => (
+                    <div key={key}>
+                      <h1>{item.title}</h1>
+                    </div>
+                  ))}
               </div>
-              )))}
-            </div>) : (<PreLoader />)
-        }/>
-      }
+            ) : (
+              <PreLoader />
+            )
+          }
+        />
+      )}
       <div className={style.FAQ}>
         <section className={style.InfosFaq}>
           <h1>Ola! Precisando de Ajuda?</h1>
           <div className={style.inputSearch}>
-
-            <input name="data" type="text" placeholder='Deixe aqui sua duvida...' onChange={HandleChange} />
-            <button type='submit' onClick={BtnSearch}><IoSearch /></button>
-
+            <input
+              name="data"
+              type="text"
+              placeholder="Deixe aqui sua duvida..."
+              onChange={HandleChange}
+            />
+            <button type="submit" onClick={BtnSearch}>
+              <IoSearch />
+            </button>
           </div>
-
         </section>
         <section className={style.BtnFAQ}>
-          <button className="Btn" value="usuario" onClick={handleSelect}>USER</button>
-          <button className="Btn" value="pagamento" onClick={handleSelect}>PAGAMENTO</button>
-          <button className="Btn" value="seguranca" onClick={handleSelect}>SEGURANÇA</button>
+          <button className="Btn" value="usuario" onClick={handleSelect}>
+            <i className={style.iconFaq}>
+              <FaUserCog />
+            </i>
+            <span>Problemas com Úsuario</span>
+          </button>
+          <button className="Btn" value="pagamento" onClick={handleSelect}>
+             <i className={style.iconFaq}>
+              <FaRegCheckSquare />
+            </i>
+            <span>Garantias e Seguranças</span>
+          </button>
+          <button className="Btn" value="seguranca" onClick={handleSelect}>
+          <i className={style.iconFaq}>
+              <FaRegCreditCard />
+            </i>
+            <span>Pagamentos</span>
+          </button>
+          <button className="Btn" value="seguranca" onClick={handleSelect}>
+          <i className={style.iconFaq}>
+              <FaRegComments />
+            </i>
+            <span>Suas Duvidas</span>
+          </button>
         </section>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default index
-
-
+export default index;
