9fc93bdb12d367c275ea95753912104ec037b1a4
WillianXG <wilian.mine789@gmail.com>
2023-03-28 18:16:51 -0300

    add questions

--- src/services/faq.js
+++ src/services/faq.js
@@ -1,57 +1,75 @@
 export const dataFaq = {
-    user: 
+  user:
     [
       {
-        id : 1 , 
-        title : "usuario" ,
-        question : "" ,
-        response:""
+        id: 1,
+        title: "usuario",
+        question: "Como recuperar  minha conta perdida?",
+        response: ""
       },
       {
-        id : 2 , 
-        title : "usuario" ,
-        question : "" ,
-        response:""
+        id: 2,
+        title: "usuario",
+        question: "Como faço para criar uma nova conta?",
+        response: ""
       },
       {
-        id : 3 , 
-        title : "usuario" ,
-        question : "" ,
-        response:""
+        id: 3,
+        title: "usuario",
+        question: "Estou tendo problema para acessar a minha conta",
+        response: ""
+      },
+      {
+        id: 4,
+        title: "usuario",
+        question: "Como desativar a minha conta?",
+        response: ""
+      },
+      {
+        id: 5,
+        title: "usuario",
+        question: "Como excluir  permanentemente minha  conta?",
+        response: ""
       },
     ],
-    payment: [
-      {
-        id : 1 , 
-        title : "Pagamento" ,
-        question : "" ,
-        response:"" 
-      },
-      {
-        id : 2 , 
-        title : "pagamento" ,
-        question : "" ,
-        response:"" 
-      },
-      {
-        id : 3 , 
-        title : "pagamento" ,
-        question : "" ,
-        response:"" 
-      },
-    ],
-    security: [
-      {
-        id : 1 ,
-         title : "seguranca" ,
-         question : "" , 
-         response:""
-      },
-      {
-        id : 1 , 
-        title : "seguranca" ,
-        question : "" , 
-        response:""
-      },
-    ]
-  }
+  payment: [
+    {
+      id: 1,
+      title: "Pagamento",
+      question: "Não consigo efetuar o pagamento, algo me impede,o que fazer?",
+      response: ""
+    },
+    {
+      id: 2,
+      title: "pagamento",
+      question: "Para onde meus dados estão indo?",
+      response: ""
+    },
+    {
+      id: 3,
+      title: "pagamento",
+      question: "Por que o usuário não consegue fazer o pagamento sem estar conectado a uma conta?",
+      response: ""
+    },
+  ],
+  security: [
+    {
+      id: 1,
+      title: "seguranca",
+      question: "Para onde meus Dados estão indo?",
+      response: "Todos os seus dados estão sendo armazenados e encriptografados para evitar qualquer  tipo de invasão  a sua conta, esses dados dão armazenados ao nosso banco para poder proporcionar  ao cliente(no caso voce) uma melhor navegação  dentro do nosso site."
+    },
+    {
+      id: 2,
+      title: "seguranca",
+      question: "Tenho direito a seguro de vida nas minhas visitas?",
+      response: ""
+    },
+    {
+      id: 3,
+      title: "seguranca",
+      question: "Como funciona a hospitalidade do passeio?",
+      response: ""
+    },
+  ]
+}
