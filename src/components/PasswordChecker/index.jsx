c6628cd1fbc40383cbf02be1e50539b7f34ab3e9
WillianXG <wilian.mine789@gmail.com>
2023-04-03 10:03:31 -0300

    validador

---
+++ src/components/PasswordChecker/index.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function PasswordChecker() {
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const checkPasswordStrength = (password) => {
+    if (password.length < 8) {
+      return "A senha deve ter pelo menos 8 caracteres";
+    }
+
+    if (!/\d/.test(password)) {
+      return "A senha deve conter pelo menos um número";
+    }
+
+    if (!/[A-Z]/.test(password)) {
+      return "A senha deve conter pelo menos uma letra maiúscula";
+    }
+
+    if (!/[a-z]/.test(password)) {
+      return "A senha deve conter pelo menos uma letra minúscula";
+    }
+
+    return "Senha forte!";
+  };
+
+  const handleCheckPassword = () => {
+    const result = checkPasswordStrength(password);
+    setMessage(result);
+  };
+
+  return (
+    <div>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handleChange} />
+      </label>
+      <button onClick={handleCheckPassword}>Check Password</button>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default PasswordChecker;
