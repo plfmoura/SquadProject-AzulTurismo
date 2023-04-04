e502e2a99eb9bd43a5cc213fa5c41d83058a742f
plfmoura <plfmoura96@gmail.com>
2023-04-04 14:56:47 -0300

    alter(word-bot): indent code

--- src/services/keyWordTracker.js
+++ src/services/keyWordTracker.js
@@ -1,38 +1,36 @@
 export const keyWorldTracker = (text) => {
   let result = "";
   if (text == "") {
-    return "Digite algo.."
-  }
-  else  if (
+    return "Digite algo..";
+  } else if (
     text.includes("usuario") ||
     text.includes("uzuario") ||
-    text.includes("user")
-     || text.includes("login")
-    || text.includes("logar") 
-    || text.includes("logado")
+    text.includes("user") ||
+    text.includes("login") ||
+    text.includes("logar") ||
+    text.includes("logado")
   ) {
     result = "usuarios";
   } else if (
-    text.includes("pagamento") || 
-    text.includes("payment") 
-    ||  text.includes("cartoês")
-    ||  text.includes("cartão") 
-    ||  text.includes("debito") 
-    ||  text.includes("credito")
+    text.includes("pagamento") ||
+    text.includes("payment") ||
+    text.includes("cartoês") ||
+    text.includes("cartão") ||
+    text.includes("debito") ||
+    text.includes("credito")
   ) {
     result = "pagamentos";
   } else if (
     text.includes("segurança") ||
     text.includes("security") ||
-    text.includes("seguranca")
-    || text.includes("garantias")  
-    || text.includes("garantia") 
-    || text.includes("vida") 
+    text.includes("seguranca") ||
+    text.includes("garantias") ||
+    text.includes("garantia") ||
+    text.includes("vida")
   ) {
     result = "seguranças";
-  }
-  else{
-    return "Desculpe, nao conseguimos encontrar sua pergunta."
+  } else {
+    return "Desculpe, nao conseguimos encontrar sua pergunta.";
   }
   return result;
 };
